c6cd2cb8f0222a66877b22dacaef548d89ca40f0
Heinti <olegukraine33@gmail.com>
2023-02-02 03:46:13 +0200

    1

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-// import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+// import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem'
 
 const ImageGallery = ({ data }) => {
   return (
