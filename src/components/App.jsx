7663c2a1942059f9f87e09407d1d2c07b06fb571
Heinti <olegukraine33@gmail.com>
2023-02-04 13:37:46 +0200

    button + cdm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,55 +5,75 @@ import { Searchbar } from './Searchbar/Searchbar';
 import { Loader } from './Loader/Loader';
 import Modal from './Modal/Modal';
 import ImageGallery from './ImageGallery/ImageGallery';
+import Button from './Button/Button';
+
 
 // const PER_PAGE = 12;
 export class App extends Component {
   state = {
-    galleryValue: [], 
+    searchQuery: '',
+    galleryValue: [],
     isLoading: false,
     imageModalUrl: '',
-
+    totalHits: 0,
+    page: 1,
     errorLoading: null,
   };
 
-  getAPIImages = async value => {
-    try {
-      // console.log(value)
-      // if (value !== Array) {
-      //  return this.setState({errorLoading: 'Wrong value, please try again'})
-      // }
-      this.setState({ isLoading: true });
-      const response = await API.getImges(value);
-console.log(response)
-      this.setState(prevState => ({
-        galleryValue: [...prevState.galleryValue, ...response],
-        
-      }));
+  async componentDidUpdate(_, prevState) {
+    const { searchQuery, page } = this.state;
+    if (prevState.page !== page || prevState.searchQuery !== searchQuery) {
+      try {
+        this.setState({ isLoading: true });
+        const response = await API.getImges(searchQuery, page);
+        const { hits, totalHits } = response;
 
-    } catch (error) {
-      this.setState({errorLoading: error})
-    } finally{
-      this.setState({isLoading: false})
+        this.setState(prevState => ({
+          galleryValue: [...prevState.galleryValue, ...hits],
+          totalHits,
+        }));
+      } catch (error) {
+        this.setState({ errorLoading: error });
+      } finally {
+        this.setState({ isLoading: false });
+      }
     }
+  }
+  // console.log(value)
+  // if (value !== Array) {
+  //  return this.setState({errorLoading: 'Wrong value, please try again'})
+  // }
+  loadMore =()=>{
+    this.setState(prevState =>({
+      page: prevState.page + 1,
+    }))
+  }
+
+  getAPIImages = value => {
+    this.setState({
+      searchQuery: value,
+      galleryValue: [],
+      totalHits: 0,
+      page: 1,
+    });
   };
 
   getLargeUrl = value => {
-    this.setState({ 
-      imageModalUrl: value, 
-      });
+    this.setState({
+      imageModalUrl: value,
+    });
   };
   render() {
-    const {errorLoading, galleryValue, isLoading, imageModalUrl } = this.state;
+    const { errorLoading, galleryValue, isLoading, imageModalUrl } = this.state;
 
     return (
       <div className="App">
         <Searchbar onSubmit={this.getAPIImages} isSubmiting={isLoading} />
-        {errorLoading && <p>
-          Ooops.. something goes wrong : {errorLoading}</p>}
-        {this.state.isLoading && <Loader />}
+        {errorLoading && <p>Ooops.. something goes wrong : {errorLoading}</p>}
+        {isLoading && <Loader />}
         <ImageGallery data={galleryValue} getLargeUrl={this.getLargeUrl} />
-        {imageModalUrl && <Modal imgUrl={imageModalUrl}/>}
-   
+        {imageModalUrl && <Modal imgUrl={imageModalUrl} />}
+        {galleryValue.length > 0 && <Button onLoadMore={this.loadMore}/>}
       </div>
     );
   }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,9 @@
+
+
+const Button = ({onLoadMore}) =>{
+return(
+    <button onClick={onLoadMore} type="button" className="Button">Load More</button>
+)
+}
+
+export default Button;
